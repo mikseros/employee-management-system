d6428b02a9da02028b6cde884344b80afb9b8a91
mikseros <mmadynski@gmail.com>
2022-08-06 17:35:18 +0200

    Get Employee by Id REST API

--- src/main/java/com/mikseros/controller/EmployeeController.java
+++ src/main/java/com/mikseros/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package com.mikseros.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mikseros.exception.ResourceNotFoundException;
 import com.mikseros.model.Employee;
 import com.mikseros.repository.EmployeeRepository;
 
@@ -32,5 +35,13 @@ public class EmployeeController {
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
+	}
+	
+	//get employee by ID REST API
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee doesn't exist with id: " + id));
+		return ResponseEntity.ok(employee);
 	}
 }
